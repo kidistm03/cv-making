52eecdcc59fadb83ba190971828c132d024df080
Kidist Meseret <kidistmeseret1@gmail.com>
2026-08-15 07:16:21 +0900

    add use state in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,23 @@
+import { useState } from "react";
+
 function App() {
+  const [cv, setCv] = useState({
+    personal: {
+      name: "",
+      email: "",
+      phone: "",
+      location: "",
+      linkedin: "",
+      github: ""
+    },
+
+    education: [],
+
+    experience: [],
+
+    skills: []
+  });
+
   return (
     <div>
       <h1>CV Builder</h1>
