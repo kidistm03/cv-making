b5b48c99683e715b7156ef392dd95092f45b5190
Kidist Meseret <kidistmeseret1@gmail.com>
2026-08-15 20:54:02 +0900

    update education form

--- src/components/EducationForm.jsx
+++ src/components/EducationForm.jsx
@@ -1,27 +1,34 @@
 function EducationForm({ cv, setCv }) {
+  function addEducation() {
+    const newEducation = {
+      school: "",
+      degree: "",
+      field: "",
+      startDate: "",
+      endDate: ""
+    };
+
+    setCv({
+      ...cv,
+
+      education: [
+        ...cv.education,
+        newEducation
+      ]
+    });
+  }
+
   return (
     <div>
       <h2>Education</h2>
 
-      <p>No education added yet.</p>
+      <p>Number of entries: {cv.education.length}</p>
 
-      <button>Add Education</button>
+      <button onClick={addEducation}>
+        Add Education
+      </button>
     </div>
   );
 }
-function addEducation() {
-  const newEducation = {
-    school: "",
-    degree: "",
-    field: "",
-    startDate: "",
-    endDate: "",
-  };
 
-  setCv({
-    ...cv,
-
-    education: [...cv.education, newEducation],
-  });
-}
 export default EducationForm;
