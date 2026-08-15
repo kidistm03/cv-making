8c2d156a171e61b977da011bbad82e9a3a1bacc1
Kidist Meseret <kidistmeseret1@gmail.com>
2026-08-15 22:48:46 +0900

    creating skils form

---
+++ src/components/SkillsForm.jsx
@@ -0,0 +1,13 @@
+function SkillsForm({ cv, setCv }) {
+  return (
+    <div>
+      <h2>Skills</h2>
+
+      <button>
+        Add Skill
+      </button>
+    </div>
+  );
+}
+
+export default SkillsForm;
