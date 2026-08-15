e65f631c85da859f9bbf0de4140096a6c646fe7b
Kidist Meseret <kidistmeseret1@gmail.com>
2026-08-15 22:24:50 +0900

    create experiance form

---
+++ src/components/ExperienceForm.jsx
@@ -0,0 +1,13 @@
+function ExperienceForm({ cv, setCv }) {
+  return (
+    <div>
+      <h2>Work Experience</h2>
+
+      <button>
+        Add Experience
+      </button>
+    </div>
+  );
+}
+
+export default ExperienceForm;
